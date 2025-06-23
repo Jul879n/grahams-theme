577ffd4c75d904f2d02f7ad980096e4e93a96914
Julián Araya <jul.dev03@gmail.com>
2025-06-22 22:26:42 -0400

    Add contacto template and content part; enhance contact information display and styling

--- template-parts/content.php
+++ template-parts/content.php
@@ -31,33 +31,4 @@
 
 	<?php mr_grahams_theme_post_thumbnail(); ?>
 
-	<div class="entry-content">
-		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'mr-grahams-theme' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
-
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'mr-grahams-theme' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php mr_grahams_theme_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
